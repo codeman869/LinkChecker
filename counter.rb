dd9cb7d6d012a8e852330707290890bfbd10a7b7
Cody Deckard <cjdeckard@gmail.com>
2015-06-29 15:05:02 +0000

    Initial Commit

---
+++ counter.rb
@@ -0,0 +1,7 @@
+f = File.open("POL.log")
+i = 0
+f.each_line do |line|
+    i = i+1
+    
+end
+puts i
---
+++ main.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'httparty'
+require 'pp'
+
+class POL
+    APPNAME = "EBS Time Request Link Check"
+    include HTTParty
+    base_uri 'https://www.portlandonline.com'
+    headers "User-Agent" => APPNAME
+end
+100.times do
+    response = POL.get('https://www.portlandonline.com/timereq/')
+    if response.code == 404
+        puts response.body, response.code, response.message, response.headers.inspect
+        f = File.open("POL.log","a+")
+        f.write("#{response.body} #{response.code} #{response.message} #{response.headers.inspect}\n")
+        f.close
+    end
+
+    pp response.code
+
+    sleep(10)
+end
